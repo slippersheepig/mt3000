59304ed44875c5274242b5ea51c16bf80523d3e5
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2024-06-11 22:55:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,5 @@
 rm -rf feeds/luci/applications/luci-app-passwall2/
 rm -rf feeds/packages/net/xray-core/
 rm -rf feeds/packages/net/xray-plugin/
-git clone https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git clone https://github.com/slippersheepig/openwrt-passwall2 package/luci-app-passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/luci-app-passwall-packages
